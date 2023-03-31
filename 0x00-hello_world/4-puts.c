f1efd9804af4495213b9fa43ea3caea05d4648fe
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-03-31 09:39:11 -0700

    dmfr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * return always 0(successful)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }		
