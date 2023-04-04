39879b78b4a66ed0c7c1b7236b7bd25155c0a6ce
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2023-04-04 13:26:30 -0700

    hellos

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - starting point
+ * main - Entry point
  *
- * return always 0(successful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}		
+}
