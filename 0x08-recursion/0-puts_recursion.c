7506fd3eb1093cd2dd0aeb9cc85e790e749a0657
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-06-07 13:39:51 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/* Write a function that prints a string, followed by a new line.
+*
+* return 0: success
+*
+*
+*/
+void _puts_recursion(const char *s);
+
+int main(void)
+{char s[50] = {"Puts with recursion"};
+ gets(s);
+ _puts_recursion(s);
+
+  return 0;
+}void _puts_recursion( const char *s)
+{
+ if(*s != 0){
+   printf("%c",*s);
+
+  _puts_recursion(s++);
+
+   } else _puts_recursion('\n');
+
+}
