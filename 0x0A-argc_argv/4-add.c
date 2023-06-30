6328029f70f2f875b5c9583a4c7e8ee0683f0563
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-06-30 07:37:10 -0700

    Adds +ve numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+/**
+ * check_num - check - string if there are digits
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int counts;
+
+	counts = 0;
+	while (counts < strlen(str)) /*counts string*/
+
+	{
+		if (!isdigit(str[counts])) /*check if in str there are digit*/
+		{
+			return (0);
+		}
+
+		counts++;
+	}
+	return (1);
+}
 
 /**
- * main - adds two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Prints the name of the program
+ * @argc: Counts number of arguments
+ * @argv: Arguments
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
+
 {
-        unsigned int sum, number1, number2;
 
-        if (argc < 3 || argc > 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-	if(number1 == NULL && number2 == NULL)
-        {
-          printf("0\n");
-        }
-        else
-        {
-            printf("Error\n");
-            return (1);
-        }
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-        number1 = atoi(argv[1]);
-        number2 = atoi(argv[2]);
-        sum = number1 + number2;
+	count = 1;
+	while (count < argc) /*scans through the array*/
+	{
+		if (check_num(argv[count]))
 
-        printf("%d\n", sum);
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
 
-        return (0);
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum); /*prints sum*/
+
+	return (0);
 }
