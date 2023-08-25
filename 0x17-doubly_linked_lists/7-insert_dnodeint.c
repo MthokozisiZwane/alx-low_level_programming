b2ec04b1cc5043baf4711de6f548a791a919861c
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-08-25 15:41:12 -0700

    dounly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	return (NULL);
 
 	if (current->next == NULL)
-	return add_dnodeint_end(h, n); /* Inserts at the end*/
+	return (add_dnodeint_end(h, n)); /* Inserts at the end*/
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
